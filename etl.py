2a704940d3e8c96a9ad3f53002b25fa3055ab10d
Juliana Zhu <juliana.zhu@clark.de>
2020-06-20 23:23:03 +0200

    Add requirements.txt & setup tests

--- etl.py
+++ etl.py
@@ -1,3 +1,17 @@
+"""
+This module defines an ETL pipeline that imports data song and log
+data from .json files in AWS S3 buckets into a star-schema Redshift database
+with the following structure:
+
+#####################    FACT TABLE:     ####################
+songplays   - records in log data associated with song plays
+##################### DIMENSION TABLES: #####################
+users       - users in the app
+songs       - songs in music database
+artists     - artists in music database
+time        - timestamps of records in songplays broken down into specific units
+"""
+
 import configparser
 import psycopg2
 from sql_queries import copy_table_queries, insert_table_queries
